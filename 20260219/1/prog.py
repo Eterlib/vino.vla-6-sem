2e04ba8486550820a757842c85f9747f4fbd5a6c
Vladimir Vinogradov <fes1z1515@gmail.com>
2026-03-11 13:21:22 +0300

    display tree object of commit

--- 20260219/1/prog.py
+++ 20260219/1/prog.py
@@ -24,3 +24,27 @@ if len(sys.argv) > 2:
     header, _, content = data.partition(b'\x00')
 
     print(content.decode())
+
+def show_tree(repo, tree_hash):
+
+    obj_path = repo / ".git" / "objects" / tree_hash[:2] / tree_hash[2:]
+
+    import zlib
+    data = zlib.decompress(obj_path.read_bytes())
+
+    header, _, content = data.partition(b'\x00')
+
+    i = 0
+
+    while i < len(content):
+
+        mode_end = content.find(b' ', i)
+        name_end = content.find(b'\x00', mode_end)
+
+        name = content[mode_end+1:name_end]
+
+        sha = content[name_end+1:name_end+21].hex()
+
+        print(name.decode())
+
+        i = name_end + 21
