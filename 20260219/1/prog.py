3a3f374fc7f47b4f6a7271247d61f79e6bc8e88c
Vladimir Vinogradov <fes1z1515@gmail.com>
2026-03-11 13:18:33 +0300

    last commit object of branch

--- 20260219/1/prog.py
+++ 20260219/1/prog.py
@@ -1,9 +1,26 @@
 import sys
+import zlib
 from pathlib import Path
 
 repo = Path(sys.argv[1])
 
 heads = repo / ".git" / "refs" / "heads"
 
-for branch in heads.iterdir():
-    print(branch.name)
+if len(sys.argv) == 2:
+    for branch in heads.iterdir():
+        print(branch.name)
+
+if len(sys.argv) > 2:
+
+    branch = sys.argv[2]
+
+    ref_path = repo / ".git" / "refs" / "heads" / branch
+    commit_hash = ref_path.read_text().strip()
+
+    obj_path = repo / ".git" / "objects" / commit_hash[:2] / commit_hash[2:]
+
+    data = zlib.decompress(obj_path.read_bytes())
+
+    header, _, content = data.partition(b'\x00')
+
+    print(content.decode())
